28331949d075ae1b5c8a31adf4f6054102881fb7
Melkeraug <54627523+Melkeraug@users.noreply.github.com>
2020-10-23 12:57:01 +0200

    jj

---
+++ Hello World/random.js
@@ -0,0 +1,38 @@
+const { read } = require('fs')
+const { listeners } = require('process')
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+let random = Math.floor(Math.random() * 100) +1
+
+let tries = 5
+
+console.log("spela då din feta fkn griiiiiiiiiiiiiiis");
+
+rl.on('line', (input)=>{
+    let answer = parseInt(input)
+    tries--;
+
+
+if (tries == 0) {
+    console.log("you lost fkn noob out played out skilled out matched")
+    process.exit()
+}
+
+else if (answer == random) {
+    console.log("u won only luck no skill lmao boosted")
+    process.exit()
+}
+
+else if (answer > random){
+    console.log("To high like my rank fkn retard get good")
+}
+
+else if(answer < random){
+    console.log("To low just like your rank silver lmao xd :PpPpPpP")
+}
+})
